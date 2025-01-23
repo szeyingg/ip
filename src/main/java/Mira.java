3844438f6a8d96e6b4a6556a6bc78a8f4341449a
szeyingg <leeszeyings@gmail.com>
2025-01-24 02:36:14 +0800

    Level-0

---
+++ src/main/java/Mira.java
@@ -0,0 +1,16 @@
+public class Mira {
+    public static void line() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        line();
+        System.out.print("""
+                Hello! I'm Mira
+                What can I do for you?
+                """);
+        line();
+        System.out.println("Bye. Hope to see you again soon!");
+        line();
+    }
+}
