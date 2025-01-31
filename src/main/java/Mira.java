39b872f44f422c95e9691b43f93927255e40ea1c
szeyingg <leeszeyings@gmail.com>
2025-02-01 02:42:58 +0800

    implemeted task class, added ability to mark as done/not

--- src/main/java/Mira.java
+++ src/main/java/Mira.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Mira {
-    private static String[] list;
+    private static Task[] list;
 
     public static void line() {
         System.out.println("____________________________________________________________");
@@ -19,16 +19,14 @@ public class Mira {
 
     public static void exit() {
         System.out.println("And... Poof! I'm off~ But don’t worry, I’m just a spell away!");
-        line();
     }
 
     public static void addTask(String command) {
         System.out.println("New task conjured: " + command);
         int idx = list.length;
-        String[] newList = Arrays.copyOf(list, idx+1);
-        newList[idx] = command;
+        Task[] newList = Arrays.copyOf(list, idx+1);
+        newList[idx] = new Task(command);
         list = newList;
-        line();
     }
 
     public static void printList() {
@@ -37,23 +35,50 @@ public class Mira {
         } else {
             System.out.println("Abra-Cadabra! Here's your task list:");
             for (int i = 1; i < list.length + 1; i++) {
-                System.out.println(i + "." + list[i - 1]);
+                Task t = list[i-1];
+                System.out.println(i + ". [" + t.getStatusIcon() + "] " + t.description);
             }
         }
-        line();
     }
+
+    public static void markTask(int idx) {
+        Task t = list[idx - 1];
+        t.markAsDone();
+        System.out.println("Presto, you did it! Task " + idx + " is done and dusted!");
+    }
+
+    public static void unmarkTask(int idx) {
+        Task t = list[idx -1];
+        t.markAsUndone();
+        System.out.println("Tough luck! Task " + idx + " has come back stronger!");
+    }
+
 
     public static void main(String[] args) {
         intro();
-        list = new String[0];
+        list = new Task[0];
         Scanner input = new Scanner(System.in);
         while (true) {
-            String command = input.nextLine();
-            switch (command) {
+            String in = input.nextLine();
+            String[] command = in.split(" ");
+            String action = command[0];
+            int param = -1;
+            //if (command.length >1)
+
+            switch (action) {
                 case "bye": exit(); break;
                 case "list": printList(); break;
-                default: addTask(command);
+                case "mark":
+                    param = Integer.parseInt(command[1]);
+                    markTask(param);
+                    break;
+                case "unmark":
+                    param = Integer.parseInt(command[1]);
+                    unmarkTask(param);
+                    break;
+                default: addTask(in);
             }
+            line();
         }
     }
 }
