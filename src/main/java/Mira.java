a92f6bab9ff37da2b046ca257111f66d59934a68
szeyingg <leeszeyings@gmail.com>
2025-01-31 17:29:26 +0800

    Level-1

--- src/main/java/Mira.java
+++ src/main/java/Mira.java
@@ -1,16 +1,42 @@
+import java.util.Scanner;
+
 public class Mira {
     public static void line() {
         System.out.println("____________________________________________________________");
     }
 
-    public static void main(String[] args) {
+    public static void intro() {
         line();
         System.out.print("""
                 Hello! I'm Mira
                 What can I do for you?
                 """);
         line();
+    }
+
+    public static void exit() {
+        line();
         System.out.println("Bye. Hope to see you again soon!");
         line();
+    }
+
+    public static void echo(String command) {
+        line();
+        System.out.println(command);
+        line();
+    }
+
+    public static void main(String[] args) {
+        intro();
+
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String command = input.nextLine();
+            if (command.equals("bye")) {
+                exit();
+            } else {
+                echo(command);
+            }
+        }
     }
 }
